b54fc05f56907d9ca0cc97ca03f4055469b3ae33
Kangonga <guchu413@gmail.com>
2023-03-08 01:21:32 +0300

    feat: add feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,17 @@
-import { Box } from "@mui/material";
+import { ExpandMore, Favorite, MoreVert, Share, ShareLocationOutlined } from "@mui/icons-material";
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, IconButton, styled, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { useState } from "react";
+import Post from "./Post";
 
 export default function Feed(){
     return (
-        <Box flex={2} bgcolor={"pink"}
-        >
-                <h1>Feed</h1>
+        <Box padding={1} flex={2} bgcolor={"black"} display="flex" flexDirection="column" alignItems="center" gap={2}>
+            <h1>Feed</h1>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
         </Box>
     )
 }
